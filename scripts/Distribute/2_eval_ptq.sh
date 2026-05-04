a4596fa180d36f07c5baffa390a4ea434285d676
kimjoohyungsd <jhkcool97@naver.com>
2026-05-04 07:46:16 +0000

    2026-05-04

--- scripts/Distribute/2_eval_ptq.sh
+++ scripts/Distribute/2_eval_ptq.sh
@@ -48,14 +48,14 @@ for CONFIG in "${BIT_CONFIGS[@]}"; do
     #     > "${OUTPUT_DIR}/log_W${W_BIT}A${A_BIT}_All_Rotations.txt" 2>&1
 
     # 실험 2: No R4 Option 실행
-    echo "Running Experiment 2: No R4 (W${W_BIT}A${A_BIT})"
-    CUDA_VISIBLE_DEVICES=$CUDA_DEVICES python ptq.py \
-        --input_model "$MODEL_PATH" \
-        --do_train False --do_eval True --per_device_eval_batch_size 2 --model_max_length 2048 --fp16 False --bf16 True --save_safetensors False \
-        --w_bits $W_BIT --a_bits $A_BIT --k_bits 16 --v_bits 16 \
-        --k_asym --v_asym --a_asym --k_groupsize 128 --v_groupsize 128 \
-        --rotate --distribute --online_r2 --deactivate_r4 --wikitext2  --w_rtn --w_clip \
-        > "${OUTPUT_DIR}/log_W${W_BIT}A${A_BIT}_No_R4.txt" 2>&1
+    # echo "Running Experiment 2: No R4 (W${W_BIT}A${A_BIT})"
+    # CUDA_VISIBLE_DEVICES=$CUDA_DEVICES python ptq.py \
+    #     --input_model "$MODEL_PATH" \
+    #     --do_train False --do_eval True --per_device_eval_batch_size 2 --model_max_length 2048 --fp16 False --bf16 True --save_safetensors False \
+    #     --w_bits $W_BIT --a_bits $A_BIT --k_bits 16 --v_bits 16 \
+    #     --k_asym --v_asym --a_asym --k_groupsize 128 --v_groupsize 128 \
+    #     --rotate --distribute --online_r2 --deactivate_r4 --wikitext2  --w_rtn --w_clip \
+    #     > "${OUTPUT_DIR}/log_W${W_BIT}A${A_BIT}_No_R4.txt" 2>&1
 
     # # 실험 3: No R2 Option 실행
     # echo "Running Experiment 3: No R2 (W${W_BIT}A${A_BIT})"
@@ -97,14 +97,14 @@ for CONFIG in "${BIT_CONFIGS[@]}"; do
         > "${OUTPUT_DIR}/log_W${W_BIT}A${A_BIT}_No_R4_R1.txt" 2>&1
 
     # 실험 5: No Rotations Option 실행
-    echo "Running Experiment 5: No Rotations (W${W_BIT}A${A_BIT})"
-    CUDA_VISIBLE_DEVICES=$CUDA_DEVICES python ptq.py \
-        --input_model "$MODEL_PATH" \
-        --do_train False --do_eval True --per_device_eval_batch_size 2 --model_max_length 2048 --fp16 False --bf16 True --save_safetensors False \
-        --w_bits $W_BIT --a_bits $A_BIT --k_bits 16 --v_bits 16 \
-        --k_asym --v_asym --a_asym --k_groupsize 128 --v_groupsize 128 \
-        --distribute --wikitext2 --w_rtn --w_clip \
-        > "${OUTPUT_DIR}/log_W${W_BIT}A${A_BIT}_No_Rotations.txt" 2>&1
+    # echo "Running Experiment 5: No Rotations (W${W_BIT}A${A_BIT})"
+    # CUDA_VISIBLE_DEVICES=$CUDA_DEVICES python ptq.py \
+    #     --input_model "$MODEL_PATH" \
+    #     --do_train False --do_eval True --per_device_eval_batch_size 2 --model_max_length 2048 --fp16 False --bf16 True --save_safetensors False \
+    #     --w_bits $W_BIT --a_bits $A_BIT --k_bits 16 --v_bits 16 \
+    #     --k_asym --v_asym --a_asym --k_groupsize 128 --v_groupsize 128 \
+    #     --distribute --wikitext2 --w_rtn --w_clip \
+    #     > "${OUTPUT_DIR}/log_W${W_BIT}A${A_BIT}_No_Rotations.txt" 2>&1
 
 done
 
